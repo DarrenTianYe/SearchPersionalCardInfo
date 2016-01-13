428d476fd1e1eaf0c8e11008770ce5ca6236cc50
tianxuhong <tianxuhong@neolink.com.cn>
2016-01-13 14:18:17 +0800

    add the project about search tool

---
+++ README.md
@@ -0,0 +1,1 @@
+#this a software of search the persional card info , phone card info , zip code;
---
+++ SearchPersionalCardInfo/src/com/example/searchpersionalcardinfo/DataListHolder.java
@@ -0,0 +1,9 @@
+package com.example.searchpersionalcardinfo;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DataListHolder {
+	public ImageView imgView;
+	public TextView textView;
+}
